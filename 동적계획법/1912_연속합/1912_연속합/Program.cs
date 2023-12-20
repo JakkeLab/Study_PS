8745aa84d2e0e522b0cef308df279f41f6fa5349
JakkeLab <itsjakk3@gmail.com>
2023-12-21 00:00:57 +0900

    DP

---
+++ 동적계획법/1912_연속합/1912_연속합/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var cnt = Console.ReadLine();
+            var nums = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToList();
+            var accumMax = GetMaxSum(nums);
+            Console.WriteLine(accumMax);
+        }
+
+        public static int GetMaxSum(List<int> nums)
+        {
+            //연속합 저장공간과 연속합에서의 최대값, 최솟값 저장
+            var res = new List<int>();
+            int maxAccum = nums[0];
+            int minAccum = nums[0];
+            int maxSole = nums.Max();
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (i == 0)
+                {
+                    res.Add(nums[0]);
+                }
+                else
+                {
+                    var accum = res[i - 1] + nums[i];
+
+                    //방금 구한 누적합이 최대값보다 큰 경우 최대값으로 할당
+                    if (accum > maxAccum)
+                    {
+                        maxAccum = accum;
+                    }
+
+                    //지금까지의 누적합 중 가장 작은 값이 최소 누적합보다 작은 경우 최솟값으로 할당
+                    if (accum < minAccum)
+                    {
+                        minAccum = accum;
+                    }
+
+                    //방금 구한 누적합 - 최소 누적합 > 최댓값이면 최댓값 갱신.
+                    if (accum - minAccum > maxAccum)
+                    {
+                        maxAccum = accum - minAccum;
+                    }
+
+                    //누적합 추가를 여기서 해야함
+                    res.Add(accum);
+                }
+            }
+
+            maxAccum = maxSole > 0 ? maxAccum : maxSole;
+            return maxAccum;
+        }
+    }
+}
---
+++ 동적계획법/1932_정수삼각형/1932_정수삼각형/1932_정수삼각형/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _1932_정수삼각형;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var lineCnt = int.Parse(Console.ReadLine());
+        int[][] stair = new int[lineCnt][];
+        for(int i = 0; i < lineCnt; i++)
+        {
+            stair[i] = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+        }
+        int maxDist = 0;
+        
+        for(int i = 0; i < stair.Length - 1; i++)
+        {
+            
+            var currentFloor = stair[i];
+            var nextFloor = stair[i + 1];
+            MaxRoute(currentFloor, nextFloor);
+            if (i == stair.Length - 2)
+            {
+                maxDist = nextFloor.Max();
+            }
+        }
+
+        //길이가 1인 계단은 별도로 처리
+        if (stair.Length == 1)
+            maxDist = stair[0][0];
+        Console.WriteLine(maxDist);
+    }
+
+    public static void MaxRoute(int[] currentFloor, int[] nextFloor)
+    {
+        for(int i = 0; i < currentFloor.Length; i++)
+        {
+            if(i == 0)
+            {
+                //좌측 끝은 무조건 좌측 끝으로만 이동 가능
+                nextFloor[i] = currentFloor[i] + nextFloor[i];
+            }
+            else
+            {
+                //현재층의 나머지 값들은 2개의 경로를 가지므로, 둘 중 큰 값을 다음층의 같은 열에 넣어주기
+                nextFloor[i] = Math.Max(currentFloor[i-1] + nextFloor[i], currentFloor[i] + nextFloor[i]);
+            }
+        }
+        //다음 층의 맨 우측값도 좌측처럼 우측 값으로만 이동 가능
+        nextFloor[currentFloor.Length] = currentFloor.Last() + nextFloor.Last();
+    }
+}
+
